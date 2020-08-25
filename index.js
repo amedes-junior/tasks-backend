db5c2db8a9cb9d72daa612bb0c6aed5c09953a1b
Amedes Júnior <amedes@gmail.com>
2020-08-25 13:11:55 -0300

    NV - Tasks Backend -> Ago, 25 de 2020 -> Alterações nos arquivos

--- index.js
+++ index.js
@@ -3,17 +3,15 @@ const app = express()
 const db = require('./config/db')
 const consign = require('consign')
 
-
-
 consign()
-  .include('./config/passport.js')
-  .then('./config/middlewares.js')
-  .then('./api')
-  .then('./config/routes.js')
-  .into(app)
+    .include('./config/passport.js')
+    .then('./config/middlewares.js')
+    .then('./api')
+    .then('./config/routes.js')
+    .into(app)
 
 app.db = db
 
 app.listen(3000, () => {
-  console.log('Backend escutando ...')
+    console.log('Backend executando...')
 })
